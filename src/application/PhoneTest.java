11cca8f4184e7dfba373fd313e45d416b79c322a
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-09-23 11:29:23 +0200

    feat: I created an object of class Charger and then called a method of class ChargeTelephone on it, setting as its parameter references to objects of class Telephone. I tested the work of the written code

--- src/application/PhoneTest.java
+++ src/application/PhoneTest.java
@@ -1,11 +1,36 @@
 package application;
 
 import model.Telephone;
+import  logic.Charger;
 
 public class PhoneTest {
 
     public static void main(String[] args) {
-        Telephone telephone = new Telephone("Samsung", "Galaxy S10", 3400, 75);
-        System.out.println(telephone.printInfo());
+        Charger charger = new Charger();
+
+        Telephone telephone1 = new Telephone("Samsung", "Galaxy S10", 3400, 75);
+        System.out.println(telephone1.printInfo());
+        charger.chargeTelephone(telephone1);
+        System.out.println(telephone1.printInfo());
+
+        Telephone phone1 = new Telephone("iphone", "pro11", 4200, 50);
+        System.out.println(phone1.printInfo());
+
+
+        charger.chargeTelephone(phone1);
+        System.out.println(phone1.printInfo());
+
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        charger.chargeTelephone(phone1);
+        System.out.println(phone1.printInfo());
+
+
+
     }
 }
