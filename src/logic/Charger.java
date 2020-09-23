cd73afc9ccfee32cef75253a37ec94d4908ae39d
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-09-23 11:24:37 +0200

    feat: I added a void method: chargeTelephone. The method gets the current battery status and sets the battery level by +1 each time it is used.

--- src/logic/Charger.java
+++ src/logic/Charger.java
@@ -1,4 +1,12 @@
 package logic;
+import model.Telephone;
 
 public class Charger {
+
+    public void chargeTelephone(Telephone phone){
+
+        phone.setChargeLevel(phone.getChargeLevel() +1);
+
+    }
+
 }
