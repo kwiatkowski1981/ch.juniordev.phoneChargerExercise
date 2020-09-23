37f96028341ef3b5479b2aedade86e1eca5fab55
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-09-23 09:28:51 +0200

    feat: Initial commit: a new exercise from javastart.pl. Make the class representing the telephone to such a form that the fields in it are private, and for each of them create a getter and setter. Additionally define a method in this class that will return information about the phone in the form of a String. The returned string should match the format displayed in the main () method above. Put the class representing the phone in the ch.juniordev.model package, and the class with the main () method in the ch.juniordev.application package.
    Also create a new class, Charger, with a charge method that accepts the phone as a parameter and charges it by 1%. This class should be included in the ch.juniordev.logic package. Test the new method by recharging the phone created in the main method.

---
+++ src/PhoneTest.java
@@ -0,0 +1,10 @@
+public class PhoneTest {
+
+    public static void main(String[] args) {
+        Telephone telephone = new Telephone("Samsung", "Galaxy S10", 3400, 75);
+        System.out.println(telephone.brand
+                + " " + telephone.model
+                + ", bateria: " + telephone.batteryCapacity +"mAh"
+                + ", poziom naładowania: " + telephone.chargeLevel + "%");
+    }
+}
---
+++ src/Telephone.java
@@ -0,0 +1,13 @@
+public class Telephone {
+    String brand;
+    String model;
+    int batteryCapacity;
+    int chargeLevel;
+
+    public Telephone(String brand, String model, int batteryCapacity, int chargeLevel) {
+        this.brand = brand;
+        this.model = model;
+        this.batteryCapacity = batteryCapacity;
+        this.chargeLevel = chargeLevel;
+    }
+}
